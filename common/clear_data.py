a153e67569958dafb2d715dbc55f09f5603462ba
wangmengmeng <wangmm>
2019-07-02 01:27:59 +0800

    done

--- common/clear_data.py
+++ common/clear_data.py
@@ -11,18 +11,18 @@ class clear_data:
         self.connect = pymysql.Connect(host=self.host, port=int(self.port), user=self.username, passwd=self.passwd, db=self.dbname,
                                charset='utf8')
         self.cur = self.connect.cursor()
-    def clear_data(self, dbname):
+    def clear_data(self):
         sql = "select CONCAT('truncate table ',  table_name, ';')  from information_schema.`TABLES` where TABLE_SCHEMA= %s"
-        self.cur.execute(sql,(dbname))
+        self.cur.execute(sql,(self.dbname))
         res = self.cur.fetchall()
         print(res)
         print(type(res))
         for s in res:
-            self.cur.execute(s)
-            print(s)
+            self.cur.execute(s[0])
+            print(s[0])
 
 if __name__ == '__main__':
     a = clear_data()
-    a.clear_data(a.dbname)
+    a.clear_data()
 
 
---
+++ run_main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019/7/1 22:57
+# @Author : wangmengmeng
+from common.clear_data import clear_data
+def run():
+    c = clear_data()
+    c.clear_data()
+
+run()
+
+
+
