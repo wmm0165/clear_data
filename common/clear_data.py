562e5e7bce15712c064e4b2824636ee9c60a4962
wangmengmeng <wmm_0165@163.com>
2019-07-02 10:03:06 +0800

    reformat code

--- common/clear_data.py
+++ common/clear_data.py
@@ -8,12 +8,14 @@ class clear_data:
     def __init__(self):
         self.host, self.port, self.username, self.passwd, self.dbname = input("请输入服务器地址,端口号,用户名，密码及数据库名，空格隔开：").split()
         # print(self.host,self.port,self.dbname)
-        self.connect = pymysql.Connect(host=self.host, port=int(self.port), user=self.username, passwd=self.passwd, db=self.dbname,
-                               charset='utf8')
+        self.connect = pymysql.Connect(host=self.host, port=int(self.port), user=self.username, passwd=self.passwd,
+                                       db=self.dbname,
+                                       charset='utf8')
         self.cur = self.connect.cursor()
+
     def clear_data(self):
         sql = "select CONCAT('truncate table ',  table_name, ';')  from information_schema.`TABLES` where TABLE_SCHEMA= %s"
-        self.cur.execute(sql,(self.dbname))
+        self.cur.execute(sql, (self.dbname))
         res = self.cur.fetchall()
         print(res)
         print(type(res))
@@ -21,8 +23,7 @@ class clear_data:
             self.cur.execute(s[0])
             print(s[0])
 
+
 if __name__ == '__main__':
     a = clear_data()
     a.clear_data()
-
-
